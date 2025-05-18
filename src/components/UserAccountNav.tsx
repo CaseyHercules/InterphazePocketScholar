16639859591fbfdd38381115c21e514d9dce7c4d
CaseyHercules <Kchholman@gmail.com>
2025-05-17 20:49:38 -0500

    fix no name state

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import { User } from "@prisma/client";
 import {
   DropdownMenu,
@@ -10,28 +10,43 @@ import {
 } from "./ui/dropdown-menu";
 import Link from "next/link";
 import UserAvatar from "./UserAvatar";
-import { signOut } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 interface UserAccountNavProps {
   user?: User;
 }
 
 const UserAccountNav: FC<UserAccountNavProps> = ({ user }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!user?.name) {
+      signOut({
+        callbackUrl: `${window.location.origin}/login`,
+      });
+    }
+  }, [user?.name]);
+
+  if (!user?.name) {
+    return null; // Don't render anything while redirecting
+  }
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
         <UserAvatar
           className="h-10 w-10"
-          user={{ name: user!.name || null, image: user?.image || null }}
+          user={{ name: user?.name || null, image: user?.image || null }}
         />
       </DropdownMenuTrigger>
       <DropdownMenuContent className="bg-background" align="end">
         <div className="flex items-start justify-start gap-2 p-2">
           <div className="flex flex-col space-y-1 leading-none">
-            {user!.name && <p className="font-medium">{user!.name}</p>}
-            {user!.email && (
+            {user?.name && <p className="font-medium">{user.name}</p>}
+            {user?.email && (
               <p className="w-[200px] truncate text-sm text-muted-foreground">
-                {user!.email}
+                {user.email}
               </p>
             )}
           </div>
